6943f52ff7973b03bb738cf7e1912921f7378c98
Furkan Ulutaş <furkanulutas@icloud.com>
2023-10-28 22:14:05 +0300

    vercel api running

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 80,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://mbk-yuzyil.furkanulutas0.repl.co/",
         changeOrigin: true,
       },
     },
