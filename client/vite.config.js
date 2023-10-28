d959e686f31d49c26b72ad6f2364f7ef1657e714
Furkan Ulutaş <furkanulutas@icloud.com>
2023-10-28 22:54:13 +0300

    port change

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 80,
+    port: 5500,
     proxy: {
       "/api": {
         target: "https://mbk-yuzyil.furkanulutas0.repl.co/",
